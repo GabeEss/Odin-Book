4c6bf9b4a163fa66902547298b0270a2696e2017
GabeEss <113389026+GabeEss@users.noreply.github.com>
2025-07-17 19:35:59 -0400

    chore: scaffold SocketContext file

---
+++ client/src/features/sockets/socket-context.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+// This context will initialize and provide a single Socket.IO connection for the user
+
+export const SocketContext = createContext();
+
+export const SocketProvider = ({children}) => {
+
+}
