4f0e424913dc37ebf46c65283789dafba86fb6ed
Gabe S <gabesliwowicz@gmail.com>
2025-08-01 13:51:48 -0400

    refactor: scaffold initializeBots to accept multiple types of bots

--- backend/bot/bot-logic.js
+++ backend/bot/bot-logic.js
@@ -1,36 +1,40 @@
 const User = require('../models/user');
+const Message = require('../models/user');
+const Event = require('../models/event');
 
-class Bot {
-    // Friendly bot responds to messages
-    static async initializeFriendlyBot(botUserId) {
-        const bot = await User.findOne({ userId: botUserId });
-        if(!bot) {
-            try {
-                console.log("No friendly bot found. Creating new bot.");
-                const newBot = new User({
-                    userId: "Friendly_Bot_01",
-                    username: "Friendly Bot",
-                    email: "friendly_bot@namebook.app",
-                    worksAt: "Name Book",
-                    livesIn: "Name Book",
-                    from: "Cyberspace",
-                    displayColor: "blue",
-                    coverColor: "yellow",
-                    friends: [],
-                    friendRequests: [],
-                    sentRequests: [],
-                    notifications: [],
-                })
-                await newBot.save();
-                return newBot;
-            } catch {
-                console.error("Failed to create bot.")
-                return;
-            }            
+class BotService {
+    static BOTS = {
+        // WELCOME BOT SENDS A MESSAGE ON SIGN UP AND RESPONDS TO MESSAGES
+        WELCOME: {
+            userId: "bot_welcome_001",
+            username: 'WelcomeBot',
+            email: 'bot.welcome@namebook.com',
+            worksAt: "Name Book",
+            livesIn: "Digital Space",
+            from: "The Cloud",
+            displayColor: "blue",
+            coverColor: "yellow",
+            friends: [],
+            friendRequests: [],
+            sentRequests: [],
+            notifications: [],
+        },
+    }
+    static async initializeBots() {
+        // Get all bot configs
+        for(const botConfig of Object.values(this.BOTS)) {
+            let bot = await User.findOne({ userId: botConfig.userId });
+            if(!bot) {
+                try {
+                    bot = new User(botConfig);
+                    await bot.save();
+                    console.log(`Create bot: ${botConfig.username}`);
+                } catch {
+                    console.error("Failed to create bot.");
+                }
+            } else console.log(`Bot exists: ${botConfig.username}`);
         }
-        console.log("Bot exists.");
-        return bot;
     }
 }
 
-module.exports = Bot;
+module.exports = BotService;
